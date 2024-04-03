f0a4cced158b040df43ad4166840b0996aec6393
hfhoffman1144 <hfh160130@utdallas.edu>
2024-04-03 17:01:58 -0500

    removed .env dependency

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-
     postgres_host: str
     postgres_user: str
     postgres_db: str
